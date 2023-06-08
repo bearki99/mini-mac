98bcc64587519cc646fe24400925a038f38e9b06
bear <424229909@qq.com>
2023-06-08 22:18:15 +0800

    feat: update register and login

--- src/components/chat/c-cpns/login.tsx
+++ src/components/chat/c-cpns/login.tsx
@@ -10,20 +10,27 @@ const Login: React.FC<IProps> = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [check, setCheck] = useState(true);
+  const [isRegister, setRegister] = useState(false);
+  const [email, setEmail] = useState("");
   const [openApp, closeApp] = useAppsStore((s) => [s.openApp, s.closeApp]);
   const Alert = useAlertStore((s) => s.useAlert);
-  const handleLogin = async () => {
+  const handleLogin = async (isRegister: boolean) => {
     try {
-      const mydata = { name: username, pwd: password };
-      const res = await request.login(mydata);
+      // 登录逻辑
+      if (!isRegister) {
+        const mydata = { name: username, pwd: password };
+        const res = await request.login(mydata);
 
-      const data = res.data as any;
-      if (res.status === 200) {
-        localStorage.setItem("token", data.token);
-        Alert("success", res.msg);
-        closeApp("login");
-        openApp("chat");
+        const data = res.data as any;
+        if (res.status === 200) {
+          localStorage.setItem("token", data.token);
+          Alert("success", res.msg);
+          closeApp("login");
+          openApp("chat");
+        }
+      } else {
       }
+
       // if (res.code === 200) {
       //   localStorage.setItem("token", data.token);
       //   localStorage.setItem("userInfo", JSON.stringify(data.userInfo));
@@ -55,64 +62,123 @@ const Login: React.FC<IProps> = () => {
           />
         </div>
         {/* 输入框 */}
-        <div className="h-[42px] flex text-black font-black flex-center">
-          <div className="h-full w-[60px] bg-white  flex-center"></div>
-          <input
-            type="text"
-            placeholder="Username"
-            className="h-full w-[130px] text-lg focus:outline-none"
-            value={username}
-            onChange={(e) => {
-              setUsername(e.target.value);
-            }}
-          />
-          <div className="h-full w-[60px] bg-white"></div>
-        </div>
-        <div className="h-[42px] flex text-black font-bold flex-center">
-          <div className="h-full w-[60px] bg-white  flex-center"> </div>
-          <input
-            type="password"
-            placeholder="Password"
-            className="h-full w-[130px] text-lg focus:outline-none"
-            value={password}
-            onChange={(e) => {
-              e.preventDefault();
-              setPassword(e.target.value);
-            }}
-          />
-          <div className="h-full w-[60px] bg-white"></div>
-        </div>
-        <div className="text-[12px] m-auto flex justify-start items-center text-[#999] flex-center">
-          <button
-            className={`flex-center w-4 h-4 border-[1px] mr-1 border-[#b2c1cb] rounded-full  text-white  ${
-              check ? "bg-[#0099ff]" : ""
-            }`}
-            onClick={() => setCheck(!check)}
-          >
-            {check ? "√" : ""}
-          </button>
-          已阅读并同意
-          <span className="text-primary hover:cursor-pointer">服务协议</span>和
-          <span className="text-primary hover:cursor-pointer">
-            隐私保护指引
-          </span>
-        </div>
+        {!isRegister ? (
+          <>
+            <div className="h-[42px] flex text-black font-black flex-center">
+              <div className="h-full w-[60px] bg-white  flex-center"></div>
+              <input
+                type="text"
+                placeholder="Username"
+                className="h-full w-[130px] text-lg focus:outline-none"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                autoComplete="off"
+              />
+              <div className="h-full w-[60px] bg-white"></div>
+            </div>
+            <div className="h-[42px] flex text-black font-bold flex-center">
+              <div className="h-full w-[60px] bg-white  flex-center"> </div>
+              <input
+                type="password"
+                placeholder="Password"
+                className="h-full w-[130px] text-lg focus:outline-none"
+                value={password}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setPassword(e.target.value);
+                }}
+                autoComplete="off"
+              />
+              <div className="h-full w-[60px] bg-white"></div>
+            </div>
+            <div className="text-[12px] m-auto flex justify-start items-center text-[#999] flex-center">
+              <button
+                className={`flex-center w-4 h-4 border-[1px] mr-1 border-[#b2c1cb] rounded-full  text-white  ${
+                  check ? "bg-[#0099ff]" : ""
+                }`}
+                onClick={() => setCheck(!check)}
+              >
+                {check ? "√" : ""}
+              </button>
+              已阅读并同意
+              <span className="text-primary hover:cursor-pointer">
+                服务协议
+              </span>
+              和
+              <span className="text-primary hover:cursor-pointer">
+                隐私保护指引
+              </span>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="h-[42px] flex text-black font-black flex-center">
+              <div className="h-full w-[60px] bg-white  flex-center"></div>
+              <input
+                type="text"
+                placeholder="Username"
+                className="h-full w-[130px] text-lg focus:outline-none"
+                value={username}
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
+                autoComplete="off"
+              />
+              <div className="h-full w-[60px] bg-white"></div>
+            </div>
+            <div className="h-[42px] flex text-black font-bold flex-center">
+              <div className="h-full w-[60px] bg-white  flex-center"> </div>
+              <input
+                type="password"
+                placeholder="Password"
+                className="h-full w-[130px] text-lg focus:outline-none"
+                value={password}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setPassword(e.target.value);
+                }}
+                autoComplete="off"
+              />
+              <div className="h-full w-[60px] bg-white"></div>
+            </div>
+            <div className="h-[42px] flex text-black font-bold flex-center">
+              <div className="h-full w-[60px] bg-white  flex-center"> </div>
+              <input
+                type="text"
+                placeholder="Email"
+                className="h-full w-[130px] text-lg focus:outline-none"
+                value={email}
+                onChange={(e) => {
+                  e.preventDefault();
+                  setEmail(e.target.value);
+                }}
+                autoComplete="off"
+              />
+            </div>
+          </>
+        )}
+
         <div className="flex-center">
           <button
             onClick={(e) => {
               e.preventDefault();
-              handleLogin();
+              handleLogin(isRegister);
             }}
             className={`rounded-lg w-[256px] h-[38px] text-white cursor-pointer ${
               !!username && !!password ? "bg-[#0099ff]" : "bg-[#a7dbfe]"
             }`}
           >
-            Log In
+            {!isRegister ? "Log In" : "Register"}
           </button>
         </div>
         <div className="w-full h-auto  pt-[20px] flex-center">
-          <span className="text-xs text-primary hover:cursor-pointer">
-            扫码登录
+          <span
+            className="text-xs text-primary hover:cursor-pointer"
+            onClick={() => setRegister(!isRegister)}
+          >
+            {!isRegister ? "用户注册" : "登录"}
           </span>
           <span className="mx-2">|</span>
           <span className="text-xs text-primary hover:cursor-pointer ">
